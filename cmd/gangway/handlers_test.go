61a2c0561306e89815adeef6503d844156c86a6b
Jim Weber <jpweber@gmail.com>
2018-02-15 14:24:02 -0500

    Test for token parser

--- cmd/gangway/handlers_test.go
+++ cmd/gangway/handlers_test.go
@@ -71,3 +71,12 @@ func TestUnauthedCommandlineHandlerRedirect(t *testing.T) {
 			status, http.StatusOK)
 	}
 }
+
+func TestParseToken(t *testing.T) {
+	idToken := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE"
+	token, _ := parseToken(idToken)
+
+	if token.Raw != idToken {
+		t.Errorf("Error parsing token. Expect raw token to be %s, but instead got %s", idToken, token.Raw)
+	}
+}
