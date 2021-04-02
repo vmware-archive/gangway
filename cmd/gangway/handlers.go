e53d0cdaaa9c5e10b0fe664e21ce6bb31dc6668d
Tyler Allen <tyler.allen@aspect.com>
2021-04-02 16:38:09 +0000

    cleanup.
    Signed-off-by: Tyler Allen <tyler.allen@aspect.com>

--- cmd/gangway/handlers.go
+++ cmd/gangway/handlers.go
@@ -182,7 +182,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// -ta keeps flow from breaking if gangway has multiple ingress points to keep state consistent
+	// keeps flow from breaking if gangway has multiple ingress points to keep state consistent
 	if cfg.AllowIngressRedirect {
 		oauth2Cfg.RedirectURL = fmt.Sprintf("https://%v/callback", r.Host)
 	}
@@ -204,21 +204,18 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	// load up session cookies
 	session, err := gangwayUserSession.Session.Get(r, "gangway")
-	fmt.Println(err)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	sessionIDToken, err := gangwayUserSession.Session.Get(r, "gangway_id_token")
-	fmt.Println(err)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	sessionRefreshToken, err := gangwayUserSession.Session.Get(r, "gangway_refresh_token")
-	fmt.Println(err)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
